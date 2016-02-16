e50b2e99d41abeb19aee1dd769ed653b49734401
zazueta galvez jesus enrique <enriquezazueta.tec@gmail.com>
2016-02-15 16:47:45 -0800

    factorymethod
    un ejemplo en c#

---
+++ FactoryMethod/factorymethod_csharp_enriquezazueta/factorymethod_csharp_enriquezazueta/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace factorymethod_csharp_enriquezazueta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // An array of creators 
+    Creator[] creators = new Creator[2];
+    creators[0] = new ConcreteCreatorA();
+    creators[1] = new ConcreteCreatorB();
+    // Iterate over creators and create products 
+    foreach(Creator creator in creators)
+    {
+      Product product = creator.FactoryMethod();
+      Console.WriteLine("Created {0}", 
+        product.GetType().Name);
+    }
+    // Wait for user 
+    Console.Read();
+  }
+}
+// "Product" 
+abstract class Product
+{
+}
+// "ConcreteProductA" 
+class ConcreteProductA : Product
+{
+}
+// "ConcreteProductB" 
+class ConcreteProductB : Product
+{
+}
+// "Creator" 
+abstract class Creator
+{
+  public abstract Product FactoryMethod();
+}
+// "ConcreteCreator" 
+class ConcreteCreatorA : Creator
+{
+  public override Product FactoryMethod()
+  {
+    return new ConcreteProductA();
+  }
+}
+// "ConcreteCreator" 
+class ConcreteCreatorB : Creator
+{
+  public override Product FactoryMethod()
+  {
+    return new ConcreteProductB();
+  }
+}
+        }
+    
+
