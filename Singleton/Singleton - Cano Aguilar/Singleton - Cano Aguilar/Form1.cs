f9aa0f72298beb75d8525d972c691959fa5eb7cd
Cano Aguilar <joseluiscanoaguilar@gmail.comexitgit config --global user.name Cano>
2016-02-18 12:33:14 -0800

    Singleton Cano Aguilar

---
+++ Singleton/Singleton - Cano Aguilar/Singleton - Cano Aguilar/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Singleton___Cano_Aguilar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sinSIngletonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SinSingleton sinSin = new SinSingleton();
+            sinSin.Show();
+        }
+
+        private void conSingletonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConSingleton.Instancia.Show();
+        }
+    }
+}
