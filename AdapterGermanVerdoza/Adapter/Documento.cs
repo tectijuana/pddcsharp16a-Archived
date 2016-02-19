7fe1334cddca03abee15c00cc21ed1d08f0abc27
Germy <12211479verdozagerman@gmail.com>
2016-02-18 16:36:43 -0800

    Delete Documento.cs

--- AdapterGermanVerdoza/Adapter/Documento.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Adapter
-{
-    public interface Documento { string contenido { set; } void dibuja(); void imprime(); }
-}
