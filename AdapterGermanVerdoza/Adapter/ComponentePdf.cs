e771cb3d35641b196cbe43f17580cd345e2462c4
Germy <12211479verdozagerman@gmail.com>
2016-02-18 16:30:55 -0800

    Added files via upload
    Commit de Adapter

---
+++ AdapterGermanVerdoza/Adapter/ComponentePdf.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adapter
+{
+    class ComponentePdf
+    {
+        protected string contenido;
+        public void obtenerpdf(string contenido)
+        {
+            this.contenido = contenido;
+        }
+
+        public void prepararpfd()
+        {
+            Console.WriteLine("Mostrar PDF: Comienzo");
+        }
+
+        public void actualizarpdf()
+        {
+            Console.WriteLine("Mostrar contenido PDF: " + contenido);
+        }
+
+        public void finalizarpdf()
+        {
+            Console.WriteLine("Mostrar PDF: Fin");
+        }
+
+        public void imprimirpdf()
+        {
+            Console.WriteLine("Imprimir PDF: " + contenido);
+        }
+    }
+}
