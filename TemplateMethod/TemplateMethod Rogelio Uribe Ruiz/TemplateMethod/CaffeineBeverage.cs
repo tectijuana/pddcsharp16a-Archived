740ce5ee1e336cde6f41c678de1d6001c329f128
rogelio28 <rogelio-uribe@hotmail.com>
2016-02-24 16:33:23 -0800

    templateMethod-Rogelio-Uribe

---
+++ TemplateMethod/TemplateMethod Rogelio Uribe Ruiz/TemplateMethod/CaffeineBeverage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    public abstract class CaffeineBeverage
+    {
+        public /*sealed*/ void PrepareRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            AddCondiments();
+        }
+
+        public abstract void Brew();
+        public abstract void AddCondiments();
+
+        void BoilWater()
+        {
+            Console.WriteLine("Boiling water...");
+            Console.ReadKey();
+        }
+
+        void PourInCup()
+        {
+            Console.WriteLine("Poring into cup...");
+            Console.ReadKey();
+        }
+
+    }
+}
