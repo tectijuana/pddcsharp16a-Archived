6b6e73aa13115cb7c48150ff4eeb35b40270edf6
Magnolia Sandoval <magnolias.93@gmail.com>
2016-02-25 16:20:10 -0800

    Added files via upload

---
+++ Visitor/VisitorSandovalMagnolia/Element.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Visitor
+{
+    abstract class Element
+    {
+        public abstract void Accept(Visitor visitor);
+    }
+}
